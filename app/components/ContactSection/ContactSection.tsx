0a3ca282c6e6aaa80d621b29cf6d9e39d5117334
mequeiras <mequeirasmoreira@gmail.com>
2025-01-16 22:35:59 -0300

    fix: remove unused FaTwitter import in ContactSection

--- app/components/ContactSection/ContactSection.tsx
+++ app/components/ContactSection/ContactSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { FaGithub, FaLinkedin, FaTwitter, FaEnvelope } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 import { ContactForm } from "./ContactForm";
 
 const socialLinks = [
